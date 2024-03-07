27b5fcd91c111b7d98a58b7f5fa3b3788f9bfe2e
athphane <athphane@gmail.com>
2024-03-07 15:06:34 +0500

    - make status_slug and status_name attributes work

---
+++ src/Enums/UserStatuses.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Javaabu\Helpers\Enums;
+
+
+enum UserStatuses: string implements IsEnum
+{
+    use NativeEnumsTrait;
+
+    case APPROVED = 'approved';
+    case PENDING = 'pending';
+    case BANNED = 'banned';
+
+    /**
+     * Initialize Messages
+     */
+    public static function messages(): array
+    {
+        return [
+            self::APPROVED->value => __('Your account is approved.'),
+            self::PENDING->value  => __('Your account needs to be approved before you can access it.'),
+            self::BANNED->value   => __('Your account has been banned.'),
+        ];
+    }
+
+    public function getMessage(): string
+    {
+        return self::messages()[$this->value];
+    }
+
+    public static function getMessageFromKey(string $key): string
+    {
+        return self::messages()[$key] ?? '';
+    }
+
+    public static function labels(): array
+    {
+        return [
+            self::APPROVED->value => __("Approved"),
+            self::PENDING->value  => __("Pending"),
+            self::BANNED->value   => __("Banned"),
+        ];
+    }
+}
