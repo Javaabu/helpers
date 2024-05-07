e49ecef01b3ff04c66ae4847518a484cf977a444
Mohamed Jailam <muhammed.jailam@gmail.com>
2024-05-07 08:21:37 +0500

    Update PublishStatuses.php
    - Added labels 
    - Updated const to cases

--- src/Enums/PublishStatuses.php
+++ src/Enums/PublishStatuses.php
@@ -7,8 +7,18 @@ enum PublishStatuses: string implements IsEnum
 {
     use NativeEnumsTrait;
 
-    const DRAFT = 'draft';
-    const PENDING = 'pending';
-    const PUBLISHED = 'published';
-    const REJECTED = 'rejected';
+    case DRAFT = 'draft';
+    case PENDING = 'pending';
+    case PUBLISHED = 'published';
+    case REJECTED = 'rejected';
+
+    public static function labels(): array
+    {
+        return [
+            self::DRAFT->value     => __("Draft"),
+            self::PENDING->value   => __("Pending"),
+            self::PUBLISHED->value => __("Published"),
+            self::REJECTED->value  => __("Rejected"),
+        ];
+    }
 }
